4655d07db49dc678ba142bdecc56162a463cbff7
Johan Heander <johan@timezynk.com>
2010-12-10 18:23:43 +0100

    176: shunit2 test for sync
    Sets up users and reports on the mock, syncs these to drupal, changes
    some reports in drupal, syncs again and verifies that the changes have
    been synced to the mock.
    
    Does this for both single-threaded and two-threaded sync.
    
    Change-Id: I44e7e7ba125ec507aac7e23b345191795777d86f

---
+++ test/test_mock_drush.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DRUSH="drush -r ${PWD}/../../htdocs"
+
+setUp() {
+	${DRUSH} intellitime-mock-reset-all
+}
+
+testAddUser() {
+	USER_ID=$(${DRUSH} intellitime-mock-add-user 'kalle' 'kallepw')
+	assertNotEquals 'USER_ID not zero' 0 "$USER_ID"
+	ALL_USERS=$(${DRUSH} intellitime-mock-list-users)
+	assertEquals 'single user' "$USER_ID 'kalle' 'kallepw'" "${ALL_USERS}"
+}
+
+testAddAssignment() {
+	AID=$(${DRUSH} intellitime-mock-add-assignment 'assignment')
+	assertNotEquals 'AID not zero' 0 "$AID"
+	ALL_ASSIGNMENTS=$(${DRUSH} intellitime-mock-list-assignments)
+	assertEquals 'single assignment' "$AID 'assignment' 0" "${ALL_ASSIGNMENTS}"
+}
+
+testAddReport() {
+	USER_ID=$(${DRUSH} intellitime-mock-add-user 'kalle' 'kallepw')
+	AID=$(${DRUSH} intellitime-mock-add-assignment 'assignment')	
+	RID=$(${DRUSH} intellitime-mock-add-report $USER_ID $AID 2010 12 05 08:00 16:30 30 comment with spaces)
+	assertTrue 'RID not zero' "[ 0 -lt $RID ]"
+	ALL_REPORTS=$(${DRUSH} intellitime-mock-list-reports)
+	assertEquals 'single report' "$RID $USER_ID $AID 2010-12-05 08:00 16:30 30 'comment with spaces'" "${ALL_REPORTS}"
+}
+
+
+source ../../../external/shunit/src/shell/shunit2
---
+++ test/test_sync.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+drupal_root="${PWD}/../../htdocs"
+DRUSH="drush -r ${drupal_root}"
+
+setUp() {
+	${DRUSH} intellitime-mock-reset-all
+	${DRUSH} vset --yes tzintellitime_mock_delay_millis 0 &
+	${DRUSH} vset --yes tzintellitime_sync_interval_minutes 0 &
+	${DRUSH} vset --yes tzintellitime_mock_enable_comments 1 &
+	tmp_url=$(${DRUSH} vget tzintellitime_base_url)
+	old_base_url=${tmp_url/tzintellitime_base_url: /}
+	${DRUSH} vset --yes tzintellitime_base_url "http://localhost/IntelliplanWeb/Portal/Login.aspx" &
+
+	# Disable any existing users scheduled by live module...(Not needed when site is cleaned between test runs.)
+	${DRUSH} intellitime-set-all-active 0 &
+
+	wait
+	users=''
+	for ((i=0; i<10; i++)) ; do
+		user_id=$(${DRUSH} intellitime-mock-add-user "user$i" "userpw$i")
+		users+="$user_id "
+	done
+	wait
+	${DRUSH} intellitime-mock-schedule-all
+	ASS=$(${DRUSH} intellitime-mock-add-assignment 'assignment')
+	echo "Users: $users"
+	for user in $users ; do
+		${DRUSH} intellitime-mock-add-report $user $ASS 2010 12 05 08:00 16:30 30 comment &
+		${DRUSH} intellitime-mock-add-report $user $ASS 2010 12 06 08:00 16:30 30 another comment &
+	done
+	wait
+}
+
+tearDown() {
+	${DRUSH} intellitime-mock-reset-all
+	${DRUSH} vset --yes tzintellitime_base_url "${old_base_url//\"/}"
+
+}
+
+testSingleThreadSyncFromMock() {
+	${DRUSH} vset --yes tzintellitime_sync_parallel_threads 1
+	partition=$(${DRUSH} intellitime-partition)
+	echo ${partition} | grep -qv ";"
+	assertTrue 'single-thread partition' $?
+	assert_successful_sync "$partition"
+}
+
+testMultiThreadSyncFromMock() {
+	${DRUSH} vset --yes tzintellitime_sync_parallel_threads 2
+	partition=$(${DRUSH} intellitime-partition)
+	echo ${partition} | grep -q ";"
+	assertTrue 'multi-thread partition' $?
+	assert_successful_sync "$partition"
+}
+
+function assert_successful_sync() {
+    partition=$1
+    query="SELECT COUNT(DISTINCT nid) FROM tzreport WHERE flags != 255 AND (assignedto = "
+    query+="${partition//[,;]/ OR assignedto = })"
+
+    drupal_users="${partition//[,;]/ }"
+    assertEquals 'must have 10 users' 10 $(echo ${drupal_users} | wc -w) || return
+
+    assert_report_count "$query" 0
+
+    ${DRUSH} vset --yes tzintellitime_mock_delay_millis 400
+    run_sync
+    
+    assert_report_count "$query" 20
+
+    echo "Changing 1 report per user on drupal side"
+    for drupal_user in $drupal_users ; do
+	report_nid=$(${DRUSH} -u ${drupal_user} tzbase-list-reports |tail -n1)
+	${DRUSH} tzbase-edit-report ${report_nid}  09:00 08:35 &
+    done
+    wait
+
+    run_sync
+    
+    changed_count=$(${DRUSH} intellitime-mock-list-reports | grep -c "09:00 08:35")
+    assertEquals '10 reports should be changed in mock' 10 $changed_count
+
+    assert_report_count "$query" 20
+}
+
+function run_sync() {
+	echo "Starting sync"
+	sync_time=$(../fork_sync.sh "${drupal_root}" "http://localhost/" | grep "finished in")
+	sync_time=$(echo "$sync_time" | sed 's/[^0-9]*//g')
+	echo "Sync time: $sync_time"
+}
+
+function assert_report_count() {
+    query=$1
+    expected_count=$2
+    report_count=$(echo "$query" | ${DRUSH} sqlc | tail -n1)
+    assertEquals "should have $expected_count reports" $expected_count "$report_count"
+    echo "Report count: $report_count"
+}
+
+source ../../../external/shunit/src/shell/shunit2
