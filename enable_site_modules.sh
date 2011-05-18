75c503ced60daf1fc624d6b93f1ef8605f4d3ecf
Johan Heander <johan@timezynk.com>
2011-05-18 12:36:02 +0200

    Also reconfiguring primary links
    Change-Id: Ibe7ab9fc3c0853f996c9792ccbf28d00c5bd3eac

--- enable_site_modules.sh
+++ enable_site_modules.sh
@@ -3,3 +3,4 @@ drush pm-disable -y comment
 drush pm-enable -y admin_menu admin_menu_toolbar po_re_importer
 drush pm-enable -y timezynk_basic_feature timezynk_intelliplan_feature
 drush variable-set -y site_frontpage tzuser
+drush variable-set -y menu_primary_links_source navigation
