ff989f138aa02198ce884ba3f8377215d1c2a6b1
Johan Heander <johan@timezynk.com>
2011-05-18 12:13:55 +0200

    Script for setting up site modules
    Change-Id: I5f1f9ddbc2b3dd0f266eec8131238454d04c1e47

---
+++ enable_site_modules.sh
@@ -0,0 +1,5 @@
+drush pm-enable -y permissions_api
+drush pm-disable -y comment
+drush pm-enable -y admin_menu admin_menu_toolbar po_re_importer
+drush pm-enable -y timezynk_basic_feature timezynk_intelliplan_feature
+drush variable-set -y site_frontpage tzuser
