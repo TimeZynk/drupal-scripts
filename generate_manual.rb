9557ac57bbb246de811bd9e47bb37b86def4dbd6
Johan Heander <johan@timezynk.com>
2011-08-22 14:28:22 +0200

    Adds CSS to customer manual
    Change-Id: I4680e62e9f576c6238a1f3a3f3729639695721e9

--- generate_manual.rb
+++ generate_manual.rb
@@ -1,7 +1,17 @@
 #!/usr/bin/env ruby
 require 'redcarpet'
 puts "<!DOCTYPE html>"
-puts '<html><head><meta charset="UTF-8"></head><body>'
+puts '<html><head><meta charset="UTF-8">'
+puts '<style type="text/css">
+body {
+    font-family: Helvetica Neue;
+    font-weight: 200;
+}
+h1, h2, h3, h4, h5 {
+    font-family: Serifa Std;
+}
+puts </style>'
+puts '</head><body>'
 Dir.glob(ARGV[0] + '/**/*.markdown') do |f|
   puts Redcarpet.new(File.read(f)).to_html
 end
