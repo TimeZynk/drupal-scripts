77a36460c1fbc0b3b26d2e65ad1dbe7766baa887
Johan Heander <johan@timezynk.com>
2015-11-13 10:26:49 +0100

    Exit after processing 20 jobs
    Change-Id: I633064104b2d500fe36f8f1dab5f16be2f79030f

--- sync_worker.rb
+++ sync_worker.rb
@@ -1,7 +1,16 @@
 require 'stalker'
 include Stalker
 
+MAX_JOBS = 20
+$num_jobs = 0
+
 job 'intellitime.sync' do |args|
   users = args['users'].join(',')
   system %Q<drush "--root=#{args['root']}" "--uri=#{args['site']}" intellitime-sync "#{users}">
+
+  # Restart periodically to avoid zombie processes from the shell
+  $num_jobs += 1
+  if $num_jobs >= MAX_JOBS
+    exit
+  end
 end
