36997d6bd2f501de3c831b57609ddf6e1fa99ef7
Johan Heander <johan@timezynk.com>
2015-11-17 15:17:00 +0100

    Print idle time in workers
    Change-Id: I1bcdc77446e9ff0f40b9d1fc51bca961ee611ec1

--- sync_worker.rb
+++ sync_worker.rb
@@ -3,8 +3,12 @@ include Stalker
 
 MAX_JOBS = 20
 $num_jobs = 0
+$last_run = Time.now.to_f
 
 job 'intellitime.sync' do |args|
+  now = Time.now.to_f
+  puts "Idle for " + (now - $last_run).to_s + " seconds"
+
   users = args['users'].join(',')
   system %Q<drush "--root=#{args['root']}" "--uri=#{args['site']}" intellitime-sync "#{users}">
 
@@ -13,4 +17,7 @@ job 'intellitime.sync' do |args|
   if $num_jobs >= MAX_JOBS
     exit
   end
+
+  # Update last run timestamp
+  $last_run = Time.now.to_f
 end
