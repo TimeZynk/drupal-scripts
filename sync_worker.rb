71fee0260f07961d7d3dfd490d37b48b12909692
Johan Heander <johan@timezynk.com>
2015-12-04 23:42:21 +0100

    Handle timeout errors to avoid zombie processes
    Change-Id: I275345564e3d08e9b4db05a9fe9315252bb38bcf

--- sync_worker.rb
+++ sync_worker.rb
@@ -1,23 +1,50 @@
 require 'stalker'
+require 'timeout'
 include Stalker
 
 MAX_JOBS = 20
 $num_jobs = 0
 $last_run = Time.now.to_f
+$child = nil
 
 job 'intellitime.sync' do |args|
   now = Time.now.to_f
   puts "Idle for " + (now - $last_run).to_s + " seconds"
 
   users = args['users'].join(',')
-  system %Q<drush "--root=#{args['root']}" "--uri=#{args['site']}" intellitime-sync "#{users}">
+  $child = spawn('drush', "--root=#{args['root']}", "--uri=#{args['site']}", 'intellitime-sync', "#{users}")
+  Process::wait($child)
+  $child = nil
+
+  # Update last run timestamp
+  $last_run = Time.now.to_f
 
   # Restart periodically to avoid zombie processes from the shell
   $num_jobs += 1
   if $num_jobs >= MAX_JOBS
     exit
   end
+end
 
-  # Update last run timestamp
-  $last_run = Time.now.to_f
+error do |e, job, args|
+  puts "Caught error, terminating child processes"
+  if $child.nil?
+    exit
+  else
+    puts "Sending SIGTERM to " + $child
+    Process::kill("TERM", $child)
+    begin
+      Timeout::timeout(5) do
+        Process::wait($child)
+        $child = nil
+      end
+    rescue Timeout::Error
+      unless $child.nil?
+        puts "Sending SIGKILL to " + $child
+        Process.kill("KILL", $child)
+        Process::wait($child)
+      end
+    end
+  end
+  exit
 end
